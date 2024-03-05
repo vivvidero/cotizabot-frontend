9e0f627c5b25173e6abc76ba7ac5dbaa5867e2e7
Rodrigo Deganutti <56453875+rodridega@users.noreply.github.com>
2024-03-05 11:47:59 -0300

    delete orderProjects helper

--- src/helpers/orderProjects.ts
+++
@@ -1,7 +0,0 @@
-export const compararPorNumeroDeId = (a, b) => {
-    const numeroIdA = a.id;
-    const numeroIdB = b.id;
-
-    // Compara los números de los IDs
-    return numeroIdA - numeroIdB;
-};
